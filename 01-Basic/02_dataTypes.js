0d9ceee7ca03da517944aff41655e5ac99c52c80
PIYUSH KUMAR <piyush46k@gmail.com>
2026-08-25 19:42:11 +0000

    datatypes

---
+++ 01-Basic/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("Piyush")
+
+
+let name="Piyush"
+let age=32
+let isLoggedIn = false
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Piyush")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined);
+
