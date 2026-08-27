56713c1453bfa9361c979c8b3c93d652c767ed1d
PIYUSH KUMAR <piyush46k@gmail.com>
2026-08-27 07:16:09 +0000

    date

---
+++ 01-Basic/07_datesin.js
@@ -0,0 +1,31 @@
+ 
+ let myDate = new Date()
+//  console.log(myDate.toString())
+//  console.log(myDate.toDateString())
+//  console.log(myDate.toLocaleString())
+// console.log(myDate.toJSON())
+// console.log(typeof myDate)
+
+// let myCreateDate = new Date(2023,0,23)
+// console.log(myCreateDate.toDateString())
+// console.log(myCreateDate.toLocaleString())
+
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth()+1)
+console.log(newDate.getDay())
+
+// console.log(`${newDate.getDay()} and the time is ${newDate.getTime()}`)
+
+console.log(newDate.toLocaleString('default',{
+    weekday: "long",
+}))
